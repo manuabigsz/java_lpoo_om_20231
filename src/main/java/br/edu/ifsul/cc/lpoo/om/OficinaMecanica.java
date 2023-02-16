6a501c07a31e6a38e559e9dcead67fe8ef5c632d
20212pf.cc0010 <20212pf.cc0010@pc-106349.passofundo.ifsul.local>
2023-02-16 10:53:29 -0300

    Projeto Oficina mecanica usado em LPOO 2023_1

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/om/OficinaMecanica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.om;
+
+/**
+ *
+ * @author 20212pf.cc0010
+ */
+public class OficinaMecanica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
